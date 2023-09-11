c00c553a2adcecdfbc5a30ebcf8f9a050eed5374
林练来 <lld2669877481@163.com>
2023-09-11 17:16:12 +0800

    fix

--- global/global.go
+++ global/global.go
@@ -1,10 +1,8 @@
 package global
 
 import (
-	"github.com/gin-contrib/sessions"
 	"github.com/redis/go-redis/v9"
 	"github.com/songzhibin97/gkit/cache/local_cache"
-	"github.com/spf13/viper"
 	"go-bbs/config"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
@@ -13,14 +11,10 @@ import (
 )
 
 var (
-	DB      *gorm.DB
-	REDIS   *redis.Client
-	CONFIG  config.Server
-	VP      *viper.Viper
-	LOG     *zap.Logger
-	Session sessions.Session
-	Cookie  cookie.Store
-	//Timer   timer.Timer = timer.NewTimerTask()
-	//Concurrency_Control = &singleflight.Group{}
+	DB         *gorm.DB
+	REDIS      *redis.Client
+	CONFIG     config.Server
+	LOG        *zap.Logger
+	Cookie     cookie.Store
 	BlackCache local_cache.Cache
 )
