dc3c81e1ac84f276db0262b957ceb02875616dc4
Linotrylie <lld2669877481@163.com>
2023-09-22 01:07:23 +0800

    fix

--- app/http/middleware/prevent_duplication.go
+++ app/http/middleware/prevent_duplication.go
@@ -7,7 +7,8 @@ import (
 	"strconv"
 )
 
-// PreventDuplication 防止重复提交，对于提交的请求，做一个3秒的锁定处理
+// PreventDuplication 防止重复提交
+// TODO 对于提交的请求，做一个3秒的锁定处理
 func PreventDuplication() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		method := ctx.Request.Method
